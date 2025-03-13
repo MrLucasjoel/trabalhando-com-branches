80aa132c4c1920823c01726af99abd14171b8ffb
lucas joel <lucas.joel1628@gmail.com>
2025-03-13 10:21:09 -0300

    modificando o codigo na brach ramo1

---
+++ index.dart
@@ -0,0 +1,16 @@
+main(){
+  String nome = "Diego";
+  int idade = 39;
+}
+
+//criando uma branch = git branch nome-da-branch
+//regras de criação de nome de branch
+//  >sem acentuação
+//  >sem espaços
+//  >nomes objetivos
+
+//Listar branches = git branch
+//entrar em uma branch = git checkout nome-branch
+
+//dar push em uma branch
+//git push origin nome-branch
